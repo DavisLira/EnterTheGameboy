91e3dce8cc8a78a6da26829c0b94dd6b46fce326
Davi Lira <davis.lira08@gmail.com>
2025-11-21 18:55:00 -0300

    Init

---
+++ Assets/Objects/Props/Chest.cs
@@ -0,0 +1,33 @@
+using System.Threading;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    public Sprite closedSprite;
+    public Sprite openingSprite;
+    public Sprite openSprite;
+
+    private SpriteRenderer sr;
+    private bool opened = false;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = closedSprite;
+    }
+
+    public void Interact()
+    {
+        if (opened) return;
+
+        opened = true;
+        StartCoroutine(OpenChest());
+    }
+
+    private System.Collections.IEnumerator OpenChest()
+    {
+        sr.sprite = openingSprite;
+        yield return new WaitForSeconds(0.3f);
+        sr.sprite = openSprite;
+    }
+}
---
+++ Assets/Player/PlayerMoviment.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Chamado automaticamente pelo PlayerInput
+    public void OnMove(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+        movement = movement.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
